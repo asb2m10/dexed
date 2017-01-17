b8182c639888872d66daf0255cc988ed37b21818
Holger Wirtz <dcoredump@googlemail.com>
2017-01-17 10:24:20 +0100

    Changed flasg for compiling.

--- src/dexed.mk
+++ src/dexed.mk
@@ -5,12 +5,12 @@
 ######################################
 
 DEXED_DEPENDENCIES = lvtk
-DEXED_VERSION = ca1c9887828a6215ea58b397171d05484a4be513
+DEXED_VERSION = b579f7b8c94b85cbabcd3cb2e532a7908720a218
 DEXED_BUNDLES = dexed.lv2
 DEXED_SITE = $(call github,dcoredump,dexed,$(DEXED_VERSION))
 
 define DEXED_BUILD_CMDS
-        (cd $(@D)/src && $(TARGET_MAKE_ENV) make -j $(PARALLEL_JOBS))
+        (cd $(@D)/src && $(TARGET_MAKE_ENV) make MOD=1 -j $(PARALLEL_JOBS))
 endef
 
 define DEXED_INSTALL_TARGET_CMDS
