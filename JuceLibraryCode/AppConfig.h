fd9c4ee452bb9c8ae4bcf2c8a1af52a0cfee303b
Dawid Stawiarski <neeocf@users.noreply.github.com>
2020-03-24 16:14:41 -0400

    Disable JUCE_VST3_CAN_REPLACE_VST2 to enable builds without external SDKs

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -141,7 +141,7 @@
 // juce_audio_plugin_client flags:
 
 #ifndef    JUCE_VST3_CAN_REPLACE_VST2
- //#define JUCE_VST3_CAN_REPLACE_VST2 1
+ #define   JUCE_VST3_CAN_REPLACE_VST2 0
 #endif
 
 #ifndef    JUCE_FORCE_USE_LEGACY_PARAM_IDS
