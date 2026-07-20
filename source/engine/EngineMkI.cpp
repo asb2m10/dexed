9c619909cfe60717324bd250e22f76822628a01f
asb2m10 <asb2m10@gmail.com>
2026-07-19 23:10:16 -0400

    Fix M_PI issue

--- source/engine/EngineMkI.cpp
+++ source/engine/EngineMkI.cpp
@@ -22,9 +22,12 @@
  */
 
 #include "EngineMkI.h"
-#define _USE_MATH_DEFINES
 #include <cmath>
 #include <cstdlib>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 #include "msfa/sin.h"
 #include "msfa/exp2.h"
